ea462683c1bff73bf6d4abb363e4fc0994994975
woodiny <contact@woodiny.com>
2025-10-19 22:33:07 +0900

    project init

---
+++ src/main/java/com/woodiny/my_awesome_repository/MyAwesomeRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.woodiny.my_awesome_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAwesomeRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAwesomeRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woodiny/my_awesome_repository/MyAwesomeRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woodiny.my_awesome_repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAwesomeRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
